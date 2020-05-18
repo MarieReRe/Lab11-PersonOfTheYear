7e644618f8f6158e3993117b819482af33e80f86
Marie Antons <chefantons@gmail.com>
2020-05-18 15:17:16 -0500

    added HomeController File in Controllers folder

---
+++ PersonOfTheYear/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PersonOfTheYear.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+    }
+}
